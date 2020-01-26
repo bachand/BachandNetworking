c89d6a64f5bb9e7853275a8e03ccb857b3d0d1a5
Michael Bachand <bachand.michael@gmail.com>
2020-01-25 20:52:38 -0800

    copy standard networking code

--- Sources/BachandNetworking/BachandNetworking.swift
+++ Sources/BachandNetworking/BachandNetworking.swift
@@ -1,3 +1,95 @@
-struct BachandNetworking {
-    var text = "Hello, World!"
+import Foundation
+
+extension URLComponents {
+  /// Adds the following query items to any existing items.
+  public mutating func appendQueryItems(_ additionalItems: [URLQueryItem]) {
+    var newItems = [URLQueryItem]()
+    newItems = queryItems ?? []
+    newItems += additionalItems
+    queryItems = newItems
+  }
+}
+
+/// Creates a URL request.
+public func makeURLRequest(_ url: URL) -> URLRequest {
+  return URLRequest(url: url)
+}
+
+/// Creates a data task.
+///
+/// - Parameter urlRequest: The request to make.
+/// - Parameter session: The session that will make the request.
+/// - Parameter stringEncoding: The encoding of the returned data, which will be used to convert the data into a string.
+/// - Parameter completionHandler: The completion handler to call when the load request is complete. This handler is executed on the delegate queue of the associated `URLSession`.
+/// - Parameter data: The data returned by the server.
+/// - Parameter response: An object that provides response metadata, such as HTTP headers and status code. If you are making an HTTP or HTTPS request, the returned object is actually an `HTTPURLResponse` object.
+/// - Parameter error: An error object that indicates why the request failed, or `nil` if the request was successful.
+public func makeDataTask(
+  urlRequest: URLRequest,
+  urlSession: URLSession = .shared,
+  completionHandler: @escaping (_ data: Data?, _ urlResponse: URLResponse?, _ error: Error?) -> Void)
+  -> URLSessionDataTask
+{
+  return urlSession.dataTask(with: urlRequest) { data, urlResponse, error in
+    completionHandler(data, urlResponse, error)
+  }
+}
+
+/// A protocol for fetch data fetching network data.
+public protocol DataFetcher {
+  /// Peform the fetch.
+  ///
+  /// - Parameter urlRequest: The URL to fetch.
+  /// - Parameter completionHandler: The completion handler to call when the load request is complete. The queue on which the handler is invoked is unspecified.
+  /// - Parameter data: The data returned by the server.
+  /// - Parameter response: An object that provides response metadata, such as HTTP headers and status code. If you are making an HTTP or HTTPS request, the returned object is actually an `HTTPURLResponse` object.
+  /// - Parameter error: An error object that indicates why the request failed, or `nil` if the request was successful.
+  func fetch(_ urlRequest: URLRequest, completionHandler: @escaping (_ data: Data?, _ urlResponse: URLResponse?, _ error: Error?) -> Void)
+}
+
+/// A concrete way to fetch network data.
+public final class DefaultDataFetcher: DataFetcher {
+  public init(urlSession: URLSession) {
+    self.urlSession = urlSession
+  }
+
+  public func fetch(_ urlRequest: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+    let dataTask = urlSession.dataTask(with: urlRequest, completionHandler: completionHandler)
+    dataTask.resume()
+  }
+
+  private let urlSession: URLSession
+}
+
+/// A synchronous operation that executes a network request.
+public final class DataOperation: Operation {
+
+  public init(dataFetcher: DataFetcher, urlRequest: URLRequest) {
+    self.dataFetcher = dataFetcher
+    self.urlRequest = urlRequest
+  }
+
+  public override func main() {
+    let dispatchGroup = DispatchGroup()
+    dispatchGroup.enter()
+    dataFetcher.fetch(urlRequest) { [unowned self ](data, urlResponse, error) in
+      self.data = data
+      self.urlResponse = urlResponse
+      self.error = error
+      dispatchGroup.leave()
+    }
+    dispatchGroup.wait()
+  }
+
+  public var data: Data?
+  public var urlResponse: URLResponse?
+  public var error: Error?
+
+  private let dataFetcher: DataFetcher
+  private let urlRequest: URLRequest
+}
+
+public func makeDataOperation(urlRequest: URLRequest, urlSession: URLSession = .shared) -> DataOperation {
+  let dataFetcher = DefaultDataFetcher(urlSession: urlSession)
+  return DataOperation(dataFetcher: dataFetcher, urlRequest: urlRequest)
 }
--- Tests/BachandNetworkingTests/BachandNetworkingTests.swift
+++ Tests/BachandNetworkingTests/BachandNetworkingTests.swift
@@ -1,15 +1,130 @@
 import XCTest
 @testable import BachandNetworking
 
-final class BachandNetworkingTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BachandNetworking().text, "Hello, World!")
+// MARK: - URLComponentsTests
+
+final class URLComponentsTests: XCTestCase {
+
+  func test_appendQueryItems_whenItemsIsNil_addsItem() {
+    var urlComponents = URLComponents()
+    guard urlComponents.queryItems == nil else { fatalError() }
+    let newQueryItem = [URLQueryItem(name: "my", value: "item")]
+    urlComponents.appendQueryItems(newQueryItem)
+    guard urlComponents.queryItems == [URLQueryItem(name: "my", value: "item")] else { fatalError() }
+  }
+
+  func test_appendQueryItems_whenItemsIsEmpty_addsItem() {
+    var urlComponents = URLComponents()
+    urlComponents.queryItems = []
+    guard urlComponents.queryItems == [] else { fatalError() }
+    let newQueryItem = [URLQueryItem(name: "my", value: "item")]
+    urlComponents.appendQueryItems(newQueryItem)
+    guard urlComponents.queryItems == [URLQueryItem(name: "my", value: "item")] else { fatalError() }
+  }
+
+  func test_appendQueryItems_whenItemsExist_addsItem() {
+    var urlComponents = URLComponents()
+    urlComponents.queryItems = [URLQueryItem(name: "my", value: "item")]
+    let newQueryItem = [URLQueryItem(name: "my", value: "otherItem")]
+    urlComponents.appendQueryItems(newQueryItem)
+    let expectedQueryItems = [
+      URLQueryItem(name: "my", value: "item"),
+      URLQueryItem(name: "my", value: "otherItem")
+    ]
+    guard urlComponents.queryItems == expectedQueryItems else { fatalError() }
+  }
+}
+
+// MARK: - DataOperationTests
+
+final class DataOperationTests: XCTestCase {
+
+  func test_start_propagatesData() {
+    let mockFetcher = MockDataFetcher()
+    mockFetcher.data = Data("hello".utf8)
+    let operation = DataOperation(dataFetcher: mockFetcher, urlRequest: makeMockURLRequest())
+    operation.start()
+    guard let data = operation.data, String(data: data, encoding: .utf8) == "hello" else { fatalError() }
+  }
+
+  func test_start_propagatesURLResponse() {
+    let mockFetcher = MockDataFetcher()
+    mockFetcher.urlResponse = URLResponse(
+      url: URL(string: "www.google.com")!,
+      mimeType: "mymime",
+      expectedContentLength: 10,
+      textEncodingName: "myencoding")
+    let operation = DataOperation(dataFetcher: mockFetcher, urlRequest: makeMockURLRequest())
+    operation.start()
+    guard operation.urlResponse?.url?.absoluteString == "www.google.com" else { fatalError() }
+  }
+
+  func test_start_propagatesError() {
+    enum MyError: Error {
+      case failure
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    let mockFetcher = MockDataFetcher()
+    mockFetcher.error = MyError.failure
+    let operation = DataOperation(dataFetcher: mockFetcher, urlRequest: makeMockURLRequest())
+    operation.start()
+    guard let error = operation.error as? MyError, error == MyError.failure else { fatalError() }
+  }
+
+  // MARK: Private
+
+  private func makeMockURLRequest() -> URLRequest {
+    let mockURL = URL(string: "/my/fake/url")!
+    return URLRequest(url: mockURL)
+  }
+}
+
+// MARK: - DefaultDataFetcherTests
+
+final class DefaultDataFetcherTests {
+  // MARK: Internal
+
+  /// - Attention: This test queries the network, and thus may be flakey
+  func test_fetch_retrievesData() {
+    let url = URL(string: "http://google.com")!
+    let urlRequest = URLRequest(url: url)
+    let dataFetcher = DefaultDataFetcher(urlSession: .shared)
+    let dispatchGroup = DispatchGroup()
+    dispatchGroup.enter()
+    dataFetcher.fetch(urlRequest) { (data, urlResponse, error) in
+      // Google should be more than 100 bytes.
+      guard let data = data, data.count > 100 else { fatalError() }
+      guard error == nil else { fatalError() }
+      dispatchGroup.leave()
+    }
+    dispatchGroup.wait()
+  }
+}
+
+// MARK: - MockDataFetcher
+
+final class MockDataFetcher: DataFetcher {
+  init(queue: DispatchQueue = .init(label: "com.bachand.MockDataFetcher")) {
+    self.queue = queue
+  }
+
+  /// The data to pass to the completion handler.
+  var data: Data?
+  /// The response to pass to the completion handler.
+  var urlResponse: URLResponse?
+  /// The error to pass to the completion handler.
+  var error: Error?
+
+  private(set) var fetchCallCount = 0
+  func fetch(
+    _ urlRequest: URLRequest,
+    completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void)
+  {
+    fetchCallCount += 1
+    queue.async { [weak self] in
+      completionHandler(self?.data, self?.urlResponse, self?.error)
+    }
+  }
+
+  private let queue: DispatchQueue
 }
